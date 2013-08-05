8474f7d211d97f819666c9d60d853963a105e3df
Yokota Kenji <mail@yokotakenji.me>
2013-08-06 00:11:19 +0900

    公開用調整

--- step_1/script.js
+++ step_1/script.js
@@ -12,15 +12,16 @@
 
 // -----------------------------------------------------------------------------
 // 変数の宣言と代入
-var RSSURL = 'http://yokotakenji.me/log/?feed=rss2';
-var CONTAINER = $('#cntr');
-var feed;
+var RSSURL = 'http://yokotakenji.me/log/?feed=rss2'; // RSSのURL
+var CONTAINER = $('#cntr'); // 記事が入るhtmlタグ
+var feed; // フィード機能 今は宣言だけ。
 
 // -----------------------------------------------------------------------------
 // フィード読み込み後に実行させたい内容の定義
 var feedCallBack = function(result){
 	// feedCallBackの中でのみ使える変数
 	var i;
+	// 変数を連続して宣言するときは","区切りでvarを省略できます。最後は必ず";"になるのに注意！
 	var entries,
 		title,
 		link;
@@ -28,7 +29,7 @@ var feedCallBack = function(result){
 	// RSSを読み込んでエラーが無かった場合の処理
 	if(!result.error){
 		entries = result.feed.entries;
-		// ループ
+		// ループ (記事の数だけ)
 		for(i=0; i<entries.length; i++){
 			title = entries[i].title;
 			link = entries[i].link;
@@ -44,6 +45,8 @@ var feedCallBack = function(result){
 var apiCallBack = function(){
 	// フィードオブジェクトの作成。同時に読み込みRSSのURLを渡す
 	feed = new google.feeds.Feed(RSSURL);
+	// 読み込ませたい記事数を設定。デフォルトでは4
+	feed.setNumEntries(10);
 	// フィードの読み込みを開始。同時に読み込み後に処理させたい関数を渡す。
 	feed.load(feedCallBack);
 };
